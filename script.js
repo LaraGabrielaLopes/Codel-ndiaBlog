6dad63e4fba3f56caeaee1a78ecae53417f01454
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-01-14 16:27:36 -0300

    caixa de pesquisa

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Curtida : criar uma função que preencha o coração quando o mesmo é clicado
 const icon = document.querySelectorAll(".icon_heart");
 
 icon.forEach(icon => {
@@ -5,3 +6,19 @@ icon.forEach(icon => {
         icon.classList.toggle("filled")
     })
 })
+
+//Pesquisa : criar a funcionalidade de pesquisar a palavra digitada nos posts da blog
+
+function findWord() {
+    let searchWord = document.getElementById('content_search').value.toLowerCase();
+    let pageText = document.body.innerHTML.toLowerCase();
+
+    if (pageText.includes(searchWord)) {
+        let highlightedText = pageText.replace(new RegExp(searchWord, 'g'), '<span style="background-color: #E07B67;">$&</span');
+
+        document.body.innerHTML = highlightedText;
+        console.log(searchWord)
+    } else {
+        console.log('Termo não existe')
+    };
+};
